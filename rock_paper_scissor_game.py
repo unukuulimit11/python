b5f1ec1f821524f038829210bcb158f69664f837
Unumunkh.A <unumunkh@UnumunkhAs-MacBook-Pro.local>
2025-12-27 21:47:56 +1000

    mini project

---
+++ rock_paper_scissor_game.py
@@ -0,0 +1,22 @@
+import random as r
+choices = ("r", "p", "s")
+name = {"r": "🪨", "p": "📄", "s": "✂️"}
+while True:
+    your_choice = input("Rock, Paper, Scissor? type (r / p / s) or type 'q' to quit: ").lower()
+
+    if your_choice == "q":
+        print("Thank you for playing😇")
+        break
+    if your_choice not in choices:
+        print("Invalid choice!")
+        continue
+    computer_choice = r.choice(choices)
+    print(f"Computer chose: {name[computer_choice]}  VS  Your choice: {name[your_choice]}")
+    result = ord(computer_choice) - ord(your_choice)
+    if result == 0:
+        print("IT IS A TIE")
+    elif result == 1 or result == 2 or result == -3:
+        print("YOU WON!")
+    elif result == -1 or result == -2 or result == 3:
+        print("YOU LOST!")
+
