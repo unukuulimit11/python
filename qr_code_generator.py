a7826d48cb9ce29f99b91dd90c3a739ea3ab73cf
Unumunkh.A <unumunkh@UnumunkhAs-MacBook-Pro.local>
2025-12-29 16:28:12 +1000

    mini project

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -2,8 +2,8 @@ import qrcode as qr
 
 data = input("Enter your text or URL here: ").strip()
 filename = input("Enter your filename here: ").strip()
-your_QRCode  = qr.QRCode(box_size=10, border = 4)
-your_QRCode.add_data(data)
-image = your_QRCode.make_image(fill_color="black", back_color="white")
+your_qr  = qr.QRCode(box_size=10, border = 4)
+your_qr.add_data(data)
+image = your_qr.make_image(fill_color="black", back_color="white")
 image.save(filename)
 print(f"Your QR Code has saved as {filename}")
