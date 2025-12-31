24d77127deded14196ef8929fc66e0276c4b92b1
Unumunkh <unukuulimit11@gmail.com>
2025-12-31 14:45:06 +1000

    mini project

--- dice_rolling.py
+++ dice_rolling.py
@@ -1,7 +1,6 @@
 from random import*
 while True:
     choice = input("Roll the dice? (y or n): ").lower()
-
     if choice == "y":
         result = []
         number_of_dice = int(input("How many dice: "))
